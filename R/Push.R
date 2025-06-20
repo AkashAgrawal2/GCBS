e1f49d74527b3fb108e7f855cca5e20b7b94531e
AkashAgrawal2 <akash.samarya.agrawal@email.com>
2025-06-20 11:12:22 -0400

    Updated.

--- R/Push.R
+++ R/Push.R
@@ -1,3 +1,4 @@
+devtools::document()
 system("git add .")                         # Stage all changes
 system("git commit -m 'Updated.'")          # Commit with a message
 system("git push")                          # Push to GitHub
